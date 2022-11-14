fefa1822f202accf634b3845ac1e104745d1085f
dpaliwa <dpaliwa@ncsu.edu>
2022-11-14 12:49:24 -0500

    Gradient Descent from Scratch for LR added

--- GradientDescentforLR.py
+++ GradientDescentforLR.py
@@ -12,7 +12,7 @@ b0 = 0.0
 learning_rate = 0.01
 
 
-def gradient_descent(x,y,w,b,learning_rate):
+def gradient_descent(x,y,b1,b0,learning_rate):
     N = x.shape[0]
 
     dldb1 = 0.0
@@ -22,7 +22,7 @@ def gradient_descent(x,y,w,b,learning_rate):
         
         dldb1 += -2*xi*(yi - (b1*xi +b0))
 
-        dldb0 += -2*(yi - b1*xi)
+        dldb0 += -2*(yi - (b1*xi +b0))
 
     #update coefficients
 
@@ -32,14 +32,13 @@ def gradient_descent(x,y,w,b,learning_rate):
     return b1,b0
 
 N = x.shape[0]
-for epoch in range(1000):
-    
+for epoch in range(250):
 
-    b1,b0 = gradient_descent(x,y,0.0,0.0,0.01)
+    b1,b0 = gradient_descent(x,y,b1,b0,0.01)
 
     yhat = b1*x + b0
 
     loss = np.divide(np.sum((y-yhat)**2, axis=0), x.shape[0]) 
     print(f'{epoch} loss is {loss}, paramters b1:{b1}, b0:{b0}')
 
-print(x,y)
+print(x,"\n",y,"\n",yhat)
